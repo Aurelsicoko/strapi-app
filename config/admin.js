43293f1405dfc306cd8b0c52880e067bc4186d98
Aurélien Georget <aurelsicoko@gmail.com>
2023-03-01 09:17:14 +0100

    Add token salt

--- config/admin.js
+++ config/admin.js
@@ -5,4 +5,7 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT', 'saltsaltsalt'),
   },
+  transfer: {
+    token: env('TRANSFER_TOKEN_SALT', 'saltsaltsalt'),
+  }
 });
